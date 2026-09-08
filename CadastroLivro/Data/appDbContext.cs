8d94a57d01af754023eae741a2d97790bc0bc896
Guilherme Silva <guilhermealvarenga.ti@gmail.com>
2026-09-08 19:54:28 -0300

    Adiciona comentarios explicativos no codigo

--- CadastroLivro/Data/appDbContext.cs
+++ CadastroLivro/Data/appDbContext.cs
@@ -9,6 +9,7 @@ namespace CadastroLivro.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Conecta na instância padrão do SQL Server instalada localmente
             optionsBuilder.UseSqlServer(
                 "Server=localhost;Database=CadastroLivroDb;Trusted_Connection=True;TrustServerCertificate=True;");
         }
