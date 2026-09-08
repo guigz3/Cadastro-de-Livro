ac70bd67812e5a8cb42f0e3df11c27ac805d85ff
Guilherme Silva <guilhermealvarenga.ti@gmail.com>
2026-09-08 19:40:53 -0300

    Configura banco de dados e conexao com SQL Server

---
+++ CadastroLivro/Data/appDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CadastroLivro.Models;
+
+namespace CadastroLivro.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=CadastroLivroDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
