8f9a3eda157b42c0f54c2b97957d6f286958b1c9
Guilherme Silva <guilhermealvarenga.ti@gmail.com>
2026-09-10 01:24:51 -0300

    Adiciona campo ano de publicacao

--- CadastroLivro/Migrations/20260910042047_AddAnoPublicacao.Designer.cs
+++ CadastroLivro/Migrations/20260910042047_AddAnoPublicacao.Designer.cs
@@ -3,6 +3,7 @@ using CadastroLivro.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CadastroLivro.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260910042047_AddAnoPublicacao")]
+    partial class AddAnoPublicacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
